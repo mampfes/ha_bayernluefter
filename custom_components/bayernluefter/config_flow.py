0692fc11d2e2ef37a01823b4fb52b80e16644c56
mampfes <mampfes@users.noreply.github.com>
2023-09-16 15:30:07 +0200

    remove references to CONF_NAME

--- custom_components/bayernluefter/config_flow.py
+++ custom_components/bayernluefter/config_flow.py
@@ -7,7 +7,6 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import (
     CONF_HOST,
-    CONF_NAME,
     CONF_MAC,
 )
 from homeassistant.helpers import aiohttp_client
@@ -63,7 +62,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             # user_input[CONF_MODEL] = avr.protocol.model
             await self.async_set_unique_id(user_input[CONF_MAC])
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+            return self.async_create_entry(title=user_input[CONF_HOST], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
